3b0203d01d2b0f984d69257a3bb827b778026cda
louixlouis <shinyk92@snu.ac.kr>
2023-03-09 19:22:26 +0900

    Separate Model code.

---
+++ AutoEncoder/model.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+        self.encoder_layer = nn.Sequential(
+            nn.Conv2d(1, 8, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(8, 16, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, stride=2),
+            nn.ReLU(),
+        )
+        self.encoder_fc = nn.Sequential(
+            nn.Linear(3*3*32, 128),
+            nn.ReLU(),
+            nn.Linear(128, 4)
+        )
+        self.decoder_fc = nn.Sequential(
+            nn.Linear(4, 128),
+            nn.ReLU(),
+            nn.Linear(128, 3*3*32),
+            nn.ReLU()
+        )
+        self.decoder_layer = nn.Sequential(
+            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding=0),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(8, 1, 3, stride=2, padding=1, output_padding=1)
+        )
+        
+    def forward(self, x):
+        out = self.encoder_layer(x)
+        latent_vec = self.encoder_fc(out.view(out.shape[0], -1))
+        out = self.decoder_fc(latent_vec)
+        reconstructed = self.decoder_layer(out.view(out.shape[0], 32, 3, 3))
+        return latent_vec, reconstructed
