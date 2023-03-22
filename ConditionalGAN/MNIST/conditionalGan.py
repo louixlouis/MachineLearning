f6683b7932753b7cdb5e785e31c58bf9dd94bc64
louixlouis <shinyk92@gmail.com>
2023-03-22 19:17:32 +0900

    .

--- ConditionalGAN/MNIST/conditionalGan.py
+++ ConditionalGAN/MNIST/conditionalGan.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         torch.cuda.manual_seed_all(777)
 
     learning_rate = 0.0002
-    training_epochs = 10
+    training_epochs = 20
     batch_size = 64
 
     checkpoints_path = './checkpoints'
@@ -35,15 +35,16 @@ if __name__ == '__main__':
 
     transform = transforms.Compose([
         transforms.ToTensor(),
-        # transforms.Normalize(0.5, 0.5)
+        transforms.Normalize(0.5, 0.5)
     ])
 
     # trainset = datasets.ImageFolder(root='./data', transform=transform)
-    trainset = datasets.MNIST(root='./data', transform=transform)
+    trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     training_batches = torch.utils.data.DataLoader(
         dataset = trainset,
         batch_size = batch_size,
         shuffle = True,
+        drop_last= True,
     )
 
     generator = Generator().to(device)
@@ -60,7 +61,7 @@ if __name__ == '__main__':
     
     one_hot = torch.zeros(10, 10).scatter_(1, torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).view(10, 1), 1)
     fixed_noise_y = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9] * 10).type(torch.LongTensor)
-    fixed_noise_y = one_hot(fixed_noise_y).to(device)
+    fixed_noise_y = one_hot[fixed_noise_y].to(device)
     
     fill = torch.zeros([10, 10, 28, 28])
     for i in range(10):
@@ -74,21 +75,21 @@ if __name__ == '__main__':
             optimizerD.zero_grad()
             X = X.to(device)
             # batch * 10 * 28 * 28
-            Y_label = fill(Y_label).to(device)
+            Y_label = fill[Y_label].to(device)
             
-            prediction = discriminator(X, Y_label).view(-1)
+            prediction = discriminator(X, Y_label)
             lossD_real = loss_function(prediction, real_label)
             lossD_real.backward()
 
             # Train with fake.
             noise_z = torch.randn(batch_size, 100, 1, 1, device=device)
             noise_y = (torch.rand(batch_size, 1)*10).type(torch.LongTensor).squeeze()
+            noise_y_G = one_hot[noise_y].to(device)
+            noise_y_D = fill[noise_y].to(device)
 
             # Generate fake image.
-            noise_y = one_hot[noise_y].to(device)
-            fake_image = generator(noise_z, noise_y)
-            noise_y = fill[noise_y]
-            prediction = discriminator(fake_image.detach(), noise_y)
+            fake_image = generator(noise_z, noise_y_G)
+            prediction = discriminator(fake_image.detach(), noise_y_D)
             lossD_fake = loss_function(prediction, fake_label)
             lossD_fake.backward()
             lossD = lossD_real + lossD_fake
@@ -96,14 +97,14 @@ if __name__ == '__main__':
 
             # Update G network: maximize log(D(G(z)))
             optimizerG.zero_grad()
-            prediction = discriminator(fake_image, noise_y)
+            prediction = discriminator(fake_image, noise_y_D)
             lossG = loss_function(prediction, real_label)
             lossG.backward()
             optimizerG.step()
 
             if (iter+1) % 100 == 0:
                 fake_image = generator(fixed_noise_z, fixed_noise_y)
-                save_image(fake_image.data, os.path.join(samples_path, f'fake_sample_{epoch+1}_{iter+1}.png'), normalize=True)
+                save_image(fake_image.data, os.path.join(samples_path, f'fake_sample_{epoch+1}_{iter+1}.png'), nrow=10, padding=0, normalize=True)
                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f'
                     % (epoch+1, training_epochs, iter+1, len(training_batches), lossD.item(), lossG.item()))
         save_checkpoint(generator, 'G', optimizerG, epoch, checkpoints_path)
--- ConditionalGAN/MNIST/conditionalGanTest.py
+++ ConditionalGAN/MNIST/conditionalGanTest.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     if device == 'cuda':
         torch.cuda.manual_seed_all(777)
 
-    batch_size = 64
+    batch_size = 10
 
     checkpoints_path = './checkpoints'
     os.makedirs(checkpoints_path, exist_ok=True)
@@ -23,27 +23,22 @@ if __name__ == '__main__':
     os.makedirs(outputs_path, exist_ok=True)
 
     transform = transforms.Compose([
-        transforms.Resize(64),
-        transforms.CenterCrop(64),
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Normalize(0.5, 0.5)
     ])
 
     generator = Generator().to(device)
     checkpoint = torch.load(os.path.join(checkpoints_path, 'model_G_10.tar'))
     generator.load_state_dict(checkpoint['model'])
 
+    one_hot = torch.zeros(10, 10).scatter_(1, torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).view(10, 1), 1)
+
     # Training loop
     with torch.no_grad():
         generator.eval()
         # Generate female images.
-        noise_x = torch.randn(batch_size, 100, 1, 1, device=device)
-        noise_y = torch.randint(0, 1, (batch_size,)).to(device)
-        fake_image = generator(noise_x, noise_y)
-        save_image(fake_image, os.path.join(outputs_path, 'smaple_female.png'), normalize=True)
-        
-        # Generate male images.
-        noise_x = torch.randn(batch_size, 100, 1, 1, device=device)
-        noise_y = torch.randint(1, 2, (batch_size,)).to(device)
-        fake_image = generator(noise_x, noise_y)
-        save_image(fake_image, os.path.join(outputs_path, 'smaple_male.png'), normalize=True)
+        noise_z = torch.randn(batch_size, 100, 1, 1, device=device)
+        noise_y = torch.tensor([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]).type(torch.LongTensor)
+        noise_y_G = one_hot[noise_y].to(device)
+        fake_image = generator(noise_z, noise_y_G)
+        save_image(fake_image, os.path.join(outputs_path, 'smaple.png'), nrow=10, normalize=True)
