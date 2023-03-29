cd11e76287e540505a1bacbe0b0f59834932d503
louixlouis <shinyk92@snu.ac.kr>
2023-03-30 03:51:24 +0900

    Add StyleGAN model code.

--- StyleGAN/model.py
+++ StyleGAN/model.py
@@ -1,6 +1,51 @@
 import torch
 import torch.nn as nn
 
+class FullyConnectedLayer(nn.Module):
+    '''
+    Customized Fully connected layer
+    '''
+    def __init__(self, in_feature, out_feature, activation) -> None:
+        super(FullyConnectedLayer, self).__init__()
+        self.weight = nn.Parameter(torch.randn([out_feature, in_feature]))
+        self.bias = True,
+        self.activation = activation
+
+    def forward(self, x):
+        weight = self.weight.to(x.dtype)
+
+        out = x.matmul(weight.t())
+        return out
+
+class ConvolutionalLayer(nn.Module):
+    '''
+    Cutomized Convolutional Layer
+    '''
+    def __init__(self, in_channels, out_channels, kernel_size, bias, activation) -> None:
+        super(ConvolutionalLayer, self).__init__()
+        self.activation = activation
+    
+    def forward(self, x):
+        return x
+    
+class MappingLayer(nn.Module):
+    '''
+    Mapping Layer
+    '''
+    def __init__(self, z_dim, activation, num_layers) -> None:
+        super(MappingLayer, self).__init__()
+        self.z_dim = z_dim
+        self.num_layers = num_layers
+
+        for num in range(self.num_layers):
+            layer = FullyConnectedLayer(512, 512, activation=activation)
+    def forward(self, x):
+        return x
+
+class SynthesizeLayer(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
