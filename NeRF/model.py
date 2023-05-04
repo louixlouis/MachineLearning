9bda5af8d7d35097c16666e65c430ed5dc995d30
hyungsoon <mrlimhs25@snu.ac.kr>
2023-05-04 16:55:59 +0900

    .

--- NeRF/model.py
+++ NeRF/model.py
@@ -1,6 +1,17 @@
 import torch
 import torch.nn as nn
 
+############
+# Positional encoding
+############
+class PositionEncoder():
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        
+    def encoder(self):
+        pass
+        
+    
 class NeRF(nn.Module):
     def __init__(self, in_channels, out_channels, in_views, depth, width, skips, use_viewdirs):
         super(NeRF, self).__init__()
@@ -11,3 +22,18 @@ class NeRF(nn.Module):
         self.width = width
         self.skips = skips
         self.use_viewdirs = use_viewdirs
+        
+        # Linear layers for position.
+        self.position_layer = []
+        
+        # Linear layers for view.
+        self.view_layer = nn.Linear(in_views + width, width//2)
+        
+    def forward(self, x):
+        '''
+        torch.split(tensor, size or section, dim=0)
+        size or section = size of a single chunk or list of sizes for each chunk.
+        dim = dimension along which to split the tensor.
+        '''
+        in_position, in_view = torch.split(x, [self.in_channels, self.in_views], dim=-1)
+        
