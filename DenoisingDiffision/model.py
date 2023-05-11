d5597c87791ca3ee6a9a8635e8780a0824efaf8f
unknown <shinyk92@snu.ac.kr>
2023-05-11 12:41:54 +0900

    .

--- DenoisingDiffision/model.py
+++ DenoisingDiffision/model.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 class DenoisingDiffusion(nn.Module):
-    def __init__():
+    def __init__(self):
         super(DenoisingDiffusion, self).__init__()
-    
+        names: List[str]
+        
     def forward(self, x):
         return x
