602b4cc8872eb6aa0bcde45c74a6c1fbcc73e92a
louis.shin <shinyk92@gmail.com>
2023-05-11 16:55:49 +0900

    Update model.py

--- StyleGAN/model.py
+++ StyleGAN/model.py
@@ -30,16 +30,43 @@ class ConvolutionalLayer(nn.Module):
     
 class MappingLayer(nn.Module):
     '''
-    Mapping Layer
+    Mapping Layer.
     '''
-    def __init__(self, z_dim, activation, num_layers) -> None:
+    def __init__(self, latent_z_dim, latent_w_dim, latent_w_broadcast, num_layers, lr_multiplier, activation, normalize_z, w_scale) -> None:
+        '''
+        Parameters :
+        latent_z_dim       : dimension of latent vector z.
+        latent_w_dim       : dimension of disentangled latent w.
+        latent_w_broadcast : broadcast latent vector w as [minibatch, latent_w_broadcast] or [minibatch, latent_w_broadcast, latent_w_dim]
+        num_layers         : number of mapping layers.
+        lr_multiplier      : learning rate multiplier.
+        activation         : activation functions, 'relu' or 'lrelu'
+        normalize_z        : normalize letent vector z before feeding to layers.
+        w_scale            : enable equalized learning rate
+        '''
         super(MappingLayer, self).__init__()
-        self.z_dim = z_dim
+        self.latent_z_dim = latent_z_dim
+        self.latent_w_dim = latent_w_dim
+        self.latent_w_broadcast = latent_w_broadcast
         self.num_layers = num_layers
-
-        for num in range(self.num_layers):
-            layer = FullyConnectedLayer(512, 512, activation=activation)
+         
+        # Activation functions.
+        
+        # Mapping layers.
+        layers = []
+        if normalize_z:
+            layers.append(PixelNormLayer())
+        layers.append(FullyConnectedLayer(self.latent_z_dim, self.latent_w_dim, activation=activation)
+        layers.append(activation)
+        for num in range(1, self.num_layers):
+            layers.append(FullyConnectedLayer(self.latent_w_dim, out_channels, activation=activation))
+            layers.append(activation)
+        self.model = nn.Sequential(*layers)
+                      
     def forward(self, x):
+        out = self.model(x)
+        if self.latent_w_broadcast is not None:
+            out = out.unsqueeze(1).expand(-1, self.latent_w_broadcast, -1)
         return x
 
 class SynthesizeLayer(nn.Module):
